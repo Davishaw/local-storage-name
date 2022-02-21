a752bc017eca06eb0e8e0861433156ff7d32d0c9
Davishaw <96178287+Davishaw@users.noreply.github.com>
2022-02-21 21:31:38 +0100

    creazione local storage

---
+++ index.js
@@ -0,0 +1,60 @@
+const input = document.querySelector('.insert-data');
+const textName = document.querySelector('.text');
+const container = document.getElementById('container');
+const message = document.querySelector('.message');
+const errorMessage = document.querySelector('error-message');
+const btn = document.querySelector('.btn');
+const storedName = localStorage.getItem('name');
+
+
+
+// 
+if(input) {
+    textName.textContent = storedName;
+}
+
+
+
+// Inserisco il valore dell'input al textName
+input.addEventListener('input', (e) => {
+    const value = e.target.value;
+    textName.textContent = value;
+});
+
+
+
+// Aggiungo il nome nel localStorage
+function addLocalStorage() {
+    if(input.value !== '') {
+        localStorage.setItem('name', textName.textContent)
+    }
+    input.value = '';
+};
+
+
+btn.addEventListener('click', addLocalStorage);
+
+
+
+
+// Inserisco il messaggio di conferma 
+function addedToLocal(msg) {
+    message.textContent = msg;
+    message.style.display = 'block';
+};
+
+
+
+
+container.addEventListener('click', (e) => {
+    if(e.target.classList.contains('btn')) {
+    addedToLocal('Name successfully added to the storage')    
+    setTimeout(() => {message.style.display = 'none'}, 3000);
+    }
+});    
+
+
+function showErrorMessage(msg) {
+    errorMessage.textContent = msg; 
+}
+
